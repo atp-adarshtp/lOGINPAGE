67c363c2e0ad6e44e75939fdb4e1da60a4bf5395
Adarsh TP <atp@dewdrive.com>
2024-05-10 13:34:57 +0530

    loginPage

---
+++ src/config.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+const connect = mongoose.connect("mongodb://localhost:27017/Login-tut");
+
+// check database connected ro not
+connect.then(()=>{
+    console.log("Database connected Successfully");
+});
+connect.catch(()=>{
+    console.log("Database cannot be connected");
+});
+
+// Create a schema
+const LoginSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+});
+
+// Collection part
+const collection = new mongoose.model("users",LoginSchema);
+
+module.exports = collection;
